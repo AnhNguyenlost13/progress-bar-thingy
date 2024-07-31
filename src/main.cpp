38c25a66a45097f3fb226a81d21de5e94a18f8c6
Nya Meow <nyameowmeow@icloud.com>
2024-07-31 22:37:12 +0700

    clean up code
    Signed-off-by: Nya Meow <nyameowmeow@icloud.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,34 @@
-/**
- * Include the Geode headers.
- */
-#include "Geode/modify/Modify.hpp"
-#include "ccTypes.h"
 #include <Geode/Geode.hpp>
-#include <Geode/binding/GameManager.hpp>
-
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
-using namespace geode::prelude;
-
-
 #include <Geode/modify/PlayLayer.hpp>
 
-ccColor3B paint(){
-	ccColor3B uwu;
-	/**
-	* Options:
-	* - Default (vanilla behavior): P1 color
-	* - Case 1: P2 color
-	* - Case 2: Player glow color
-	* - Case 3: Manual (configure in settings)
-	*/
-	switch(Mod::get()->getSettingValue<int64_t>("mode")){
-		case 1: 
-			uwu = GameManager::sharedState()->colorForIdx(GameManager::sharedState()->getPlayerColor2());
-			break;
-		case 2:
-			uwu = GameManager::sharedState()->colorForIdx(GameManager::sharedState()->getPlayerGlowColor());
-			break;
-		case 3:
-			uwu = Mod::get()->getSettingValue<ccColor3B>("color");
-			break;
-		default:
-			uwu = GameManager::sharedState()->colorForIdx(GameManager::sharedState()->getPlayerColor());
-			break;
-	}
-	return uwu;
+using namespace geode::prelude;
+
+ccColor3B paint() {
+    auto gm = GameManager::sharedState(); // For convenience
+    /**
+    * Options:
+    * - Default (vanilla behavior): P1 color
+    * - Case 1: P2 color
+    * - Case 2: Player glow color
+    * - Case 3: Manual (configure in settings)
+    */
+    switch(Mod::get()->getSettingValue<int64_t>("mode")){
+        case 1: 
+            return gm->colorForIdx(gameManager->getPlayerColor2());
+        case 2:
+            return gm->colorForIdx(gameManager->getPlayerGlowColor());
+        case 3:
+            return Mod::get()->getSettingValue<ccColor3B>("color");
+        default:
+            return gm->colorForIdx(gameManager->getPlayerColor());
+    }
 }
 
 class $modify(PlayLayer) {
-	void startGame() {
-		PlayLayer::startGame();
-		if ((!m_level->isPlatformer()) && m_progressFill){
-			ccColor3B nya = paint();
-			// auto progress_bar = static_cast<CCSprite* >(m_progressFill); // fuuuuck // fuuuuuuck
-			m_progressFill->setColor(nya);
-		}
-	}
+    void startGame() {
+        PlayLayer::startGame();
+        if (!m_level->isPlatformer() && m_progressFill) {
+            m_progressFill->setColor(paint());
+        }
+    }
 };
