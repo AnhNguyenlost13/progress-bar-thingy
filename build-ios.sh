577eb109ace691dc14fb0c03eec7f99359cbb5fd
Nya Meow <nyamemeow@icloud.com>
2024-08-31 14:45:28 +0700

    v1.4.1

--- build-ios.sh
+++
@@ -1,6 +0,0 @@
-export GEODE_SDK=/Users/nyaauwu/Documents/geode-cgs
-export GEODE_BINDINGS_REPO_PATH=/Users/nyaauwu/Documents/bindings
-cmake -B build-ios -DGEODE_TARGET_PLATFORM=iOS \
-    -DCMAKE_SYSTEM_NAME=iOS \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo
-cmake --build build-ios
