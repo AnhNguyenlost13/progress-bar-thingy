30d7b762b26cd7eddf7bc95220ca589a918d4c92
Nya Meow <nyameowmeow@icloud.com>
2024-11-23 23:07:01 +0700

    girl what did you do!?!

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,95 @@
 #include <Geode/modify/PlayLayer.hpp>
 #define updateProgressBar updateProgressbar // sorry RobTop
 
-#include "util/color.hpp" // Never thought I would see this day.
+#include "util/color.hpp" // Thanks TheSillyDoggo
 
 using namespace geode::prelude;
 
 ccColor3B paint() {
     auto gm = GameManager::sharedState(); // For convenience
-    auto ms = Mod::get()->getSettingValue<int64_t>("mode"); // This is stupid.
-    /**
-    * Options:
-    * - Default (vanilla behavior): P1 color
-    * I know it's quite weird to make a mod that doesn't do its job at first
-    * - Case 1: P2 color
-    * - Case 2: Player glow color
-    * - Case 3: Manual (user)
-    * 
-    * Special cases:
-    * - Case 4: RGB
-    * - Case 5: Pastel
-    */
-    switch(ms){
-        case 1: 
-            return gm->colorForIdx(gm->getPlayerColor2());
-        case 2:
-            return gm->colorForIdx(gm->getPlayerGlowColor());
-        case 3:
-            return Mod::get()->getSettingValue<ccColor3B>("color");
-        case 4:
-            return colorutil::getChromaColour();
-        case 5:
-            return colorutil::getPastelColour();
-        case 6:
-            // Placeholder: Custom gradient mode, this may come at one point(TM)
-        default:
+    auto pm = GJBaseGameLayer::get();
+    auto pl = PlayLayer::get();
+    auto mg = Mod::get();
+
+    auto meow = mg->getSettingValue<std::string>("normal-working-mode");
+    auto uwu = mg->getSettingValue<std::string>("practice-working-mode");
+    auto nyaa = mg->getSettingValue<std::string>("enby-working-mode");
+    auto pt = mg->getSettingValue<bool>("practice-mode-toggle");
+    auto po = mg->getSettingValue<bool>("practice-override");
+
+    auto lp = as<float>(PlayLayer::get()->getCurrentPercentInt());
+    auto lb = pl->m_level->m_normalPercent;
+
+   // hello yanderedev
+   if (lp > lb) {
+    if (pm->m_isPracticeMode && po) {
+        if (uwu == "Player Col 1") {
             return gm->colorForIdx(gm->getPlayerColor());
+        } else if (uwu == "Player Col 2") {
+            return gm->colorForIdx(gm->getPlayerColor2());
+        } else if (uwu == "Player Glow") {
+            return gm->colorForIdx(gm->getPlayerGlowColor());
+        } else if (uwu == "Chroma") {
+            return colorutil::getChromaColour();
+        } else if (uwu == "Pastel") {
+            return colorutil::getPastelColour();
+        } else if (uwu == "Custom") {
+            return Mod::get()->getSettingValue<ccColor3B>("color");
+        } else if (uwu == "Gradient") {
+            // Placeholder
+        }
+    } else {
+        if (nyaa == "Player Col 1") {
+            return gm->colorForIdx(gm->getPlayerColor());
+        } else if (nyaa == "Player Col 2") {
+            return gm->colorForIdx(gm->getPlayerColor2());
+        } else if (nyaa == "Player Glow") {
+            return gm->colorForIdx(gm->getPlayerGlowColor());
+        } else if (nyaa == "Chroma") {
+            return colorutil::getChromaColour();
+        } else if (nyaa == "Pastel") {
+            return colorutil::getPastelColour();
+        } else if (nyaa == "Custom") {
+            return Mod::get()->getSettingValue<ccColor3B>("color");
+        } else if (nyaa == "Gradient") {
+            // Placeholder
+        }
     }
+   } else {
+    if (pm->m_isPracticeMode && pt) {    
+        if (uwu == "Player Col 1") {
+            return gm->colorForIdx(gm->getPlayerColor());
+        } else if (uwu == "Player Col 2") {
+            return gm->colorForIdx(gm->getPlayerColor2());
+        } else if (uwu == "Player Glow") {
+            return gm->colorForIdx(gm->getPlayerGlowColor());
+        } else if (uwu == "Chroma") {
+            return colorutil::getChromaColour();
+        } else if (uwu == "Pastel") {
+            return colorutil::getPastelColour();
+        } else if (uwu == "Custom") {
+            return Mod::get()->getSettingValue<ccColor3B>("color");
+        } else if (uwu == "Gradient") {
+            // Placeholder
+        }
+    } else {
+        if (meow == "Player Col 1") {
+            return gm->colorForIdx(gm->getPlayerColor());
+        } else if (meow == "Player Col 2") {
+            return gm->colorForIdx(gm->getPlayerColor2());
+        } else if (meow == "Player Glow") {
+            return gm->colorForIdx(gm->getPlayerGlowColor());
+        } else if (meow == "Chroma") {
+            return colorutil::getChromaColour();
+        } else if (meow == "Pastel") {
+            return colorutil::getPastelColour();
+        } else if (meow == "Custom") {
+            return Mod::get()->getSettingValue<ccColor3B>("color");
+        } else if (meow == "Gradient") {
+            // Placeholder
+        }
+    }
+   }
 }
 
 class $modify(PlayLayer) {
@@ -49,7 +103,6 @@ class $modify(PlayLayer) {
     
     void updateProgressBar() {
         PlayLayer::updateProgressBar();
-        // Please kill me.
         colorutil::update(Mod::get()->getSettingValue<double_t>("speed"));
         // colorutil::va += CCDirector::get()->getDeltaTime();
         // colorutil::update(CCDirector::get()->getDeltaTime());
@@ -57,18 +110,5 @@ class $modify(PlayLayer) {
             m_progressFill->setColor(paint());
         }
         
-        float uwu = static_cast<float>(PlayLayer::getCurrentPercentInt());
-        if (uwu > (m_level->m_normalPercent)) {
-            // later
-        }
-    }
-
-    void togglePracticeMode(bool practiceMode) {
-        PlayLayer::togglePracticeMode(practiceMode);
-        if (practiceMode == true) {
-        // hi
-        } else {
-        // meow
-        }
     }
 };
--- support.md
+++ support.md
@@ -1,1 +1,3 @@
 I would be really glad if you can buy me a pair of thigh highs or arm sleeves, but unfortunately I probably can't accept anything from strangers.
+
+A kind "meow" to me is also sufficient to make my day! I would really appreciate it if you can contribute to the code. If you have ideas, you can either open an issue on the GitHub repository or join the Discord server.
